9a676ca653e1f1a4b8d4802ef38761a6b1cdc0d8
zekrom-vale <sgraven66@gmail.com>
2017-05-14 13:42:09 -0500

    Add files via upload

--- js/move.js
+++ js/move.js
@@ -1,37 +1,29 @@
-ScrollRate = 0.00000000000000001;
-
-function scrollDiv_init(ScrollRate) {
-	DivElmnt = document.getElementById('scroll');
-	ReachedMaxScroll = false;
-	
-	DivElmnt.scrollTop = 0;
-	PreviousScrollTop  = 0;
-	
-	ScrollInterval = setInterval('scrollDiv(ScrollRate)', ScrollRate);
+var SR;
+function scrollDiv_init(SR) {
+	DivElmnt= document.getElementById('scroll');
+	ReachedMaxScroll= false;
+	DivElmnt.scrollTop= PST= 0;
+	ScrI= setInterval('ScrD(SR)', SR);
 }
-
-function scrollDiv(ScrollRate) {
+function ScrD(SR) {
 	
 	if (!ReachedMaxScroll) {
-		DivElmnt.scrollTop = PreviousScrollTop;
-		PreviousScrollTop++;
-		
-		ReachedMaxScroll = DivElmnt.scrollTop >= (DivElmnt.scrollHeight - DivElmnt.offsetHeight);
+		DivElmnt.scrollTop= PST;
+		PST++;
+		ReachedMaxScroll= DivElmnt.scrollTop>= (DivElmnt.scrollHeight - DivElmnt.offsetHeight);
 	}
 	else {
-		ReachedMaxScroll = (DivElmnt.scrollTop == 0)?false:true;
-		
-		DivElmnt.scrollTop = PreviousScrollTop;
-		PreviousScrollTop--;
+		ReachedMaxScroll= (DivElmnt.scrollTop== 0)?false:true;
+		DivElmnt.scrollTop= PST;
+		PST--;
 	}
 }
-
 function pauseDiv() {
-	clearInterval(ScrollInterval);
+	clearInterval(ScrI);
 }
-
-function resumeDiv(ScrollRate) {
-	PreviousScrollTop = DivElmnt.scrollTop;
-	ScrollInterval    = setInterval('scrollDiv(ScrollRate)', ScrollRate);
+function resumeDiv(SR) {
+	PST= DivElmnt.scrollTop;
+	ScrI= setInterval('ScrD(SR)', SR);
 }
+//Modified from...
 //https://www.sitepoint.com/community/t/auto-scrolling-a-div-with-overflow-scroll-auto/2291
